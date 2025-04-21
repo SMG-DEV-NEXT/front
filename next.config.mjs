0a8a22cb72c944ceccb4d071975ff4a4f885faa8
SMG-DEV-NEXT <smgdevelopmentnest@gmail.com>
2025-04-21 19:07:13 +0400

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -16,7 +16,7 @@ const nextConfig = {
           { key: "Access-Control-Allow-Credentials", value: "true" },
           {
             key: "Access-Control-Allow-Origin",
-            value: "https://92ad-46-36-116-211.ngrok-free.app",
+            value: "https://4c84-46-36-116-211.ngrok-free.app",
           }, // NOT *
           {
             key: "Access-Control-Allow-Methods",
