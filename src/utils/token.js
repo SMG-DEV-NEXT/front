ff03d8ad2c637350c180dfd02d1ac70e722d07a1
SMG-DEV-NEXT <smgdevelopmentnest@gmail.com>
2025-07-13 02:53:49 +0400

    changes

--- src/utils/token.js
+++ src/utils/token.js
@@ -12,6 +12,8 @@ export const removeAccessToken = () => {
   if (typeof window !== "undefined") {
     sessionStorage.removeItem(ACCESS_TOKEN);
   }
+  localStorage.removeItem("anon_user_id");
+
   return Cookies.remove(ACCESS_TOKEN);
 };
 
@@ -23,5 +25,6 @@ export const setAccessToken = (token, isRemember) => {
   if (!isRemember) {
     return sessionStorage.setItem(ACCESS_TOKEN, token);
   }
+  localStorage.removeItem("anon_user_id");
   return Cookies.set(ACCESS_TOKEN, token);
 };
